4eebf34a0f74a8df5018307678e3c0be763698b8
tchamp1912 <thomascampion@outlook.com>
2021-03-28 15:26:41 -0400

    update the image topic for qr decode

--- src/camera_pkg/src/camera_pkg_node.cpp
+++ src/camera_pkg/src/camera_pkg_node.cpp
@@ -11,7 +11,7 @@ int main( int argc, char* argv[] )
 
    ros::init(argc, argv, "camera_pkg_node");
    ros::NodeHandle n;
-   ros::Subscriber img_sub = n.subscribe("/camera/rgb/image_raw", 10, &Camera::imageCallback, &cam);
+   ros::Subscriber img_sub = n.subscribe("/rgb_raw", 10, &Camera::imageCallback, &cam);
    ros::Publisher qr_pub = n.advertise<std_msgs::String>("qr_code", 10);
    ros::Rate loop_rate(50);
 
